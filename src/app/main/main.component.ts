0aa6855e5652d16300fd207f2212b732351cba8c
jmugure95 <jmugure95@gmail.com>
2021-07-05 20:57:26 +0300

    add incidence markers

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,21 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { IncidenceService } from '../incidence.service';
 
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -30,7 +45,7 @@ export class MainComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.initMap();
-    // this.incidenceService.getIncidences(this.map);
+    this.incidenceService.getIncidences(this.map);
   }
 
 }
